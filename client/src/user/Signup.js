892110c852177111154505c207fa84378e060f47
LK <lk@alienbuild.uk>
2020-10-03 11:47:57 +0100

    Add styles to login/signup page

--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -2,6 +2,9 @@ import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import Default from '../layouts/default/Default';
 import { signup } from "../auth";
+import SpitbossLogo from "../assets/images/spitboss.svg";
+import Form from "react-bootstrap/cjs/Form";
+import Button from "react-bootstrap/cjs/Button";
 
 const Signup = () => {
 
@@ -42,42 +45,53 @@ const Signup = () => {
     // Signup form markup
     const signUpForm = () => (
         <div className={"container"}>
-            <form>
-                <div>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" name="name" onChange={handleChange('name')} value={name} />
-                </div>
-                <div>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="email" onChange={handleChange('email')} value={email} />
-                </div>
-                <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password" onChange={handleChange('password')} value={password} />
-                </div>
-                <div>
-                    <button type="submit" onClick={(e) => clickSubmit(e)}>Submit</button>
-                </div>
+            <form className={`signup__form`}>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label className={`signup__label`}>Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter name" onChange={handleChange('name')} value={name} />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label className={`signup__label`}>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" onChange={handleChange('email')} value={email} />
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label className={`signup__label`}>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" onChange={handleChange('password')} value={password} />
+                </Form.Group>
+
+                <Button variant="primary" type="submit" className={`signup__submit`} onClick={(e) => clickSubmit(e)}>
+                    Register
+                </Button>
             </form>
         </div>
     );
 
     // Handle errors
     const showError = () => (
-        <div className="alert alert-danger" style={{display: error ? '' : 'none'}}>{error}</div>
+        <div className="alert alert-danger signup__alert" style={{display: error ? '' : 'none'}}>{error}</div>
     );
 
     // Handle success
     const showSuccess = () => (
-        <div className="alert info" style={{display: success ? '' : 'none'}}>New user created. <Link to="/signin">Please sign in.</Link></div>
+        <div className="alert info signup__alert" style={{display: success ? '' : 'none'}}>New user created. <Link to="/signin">Please sign in.</Link></div>
     );
 
     return(
-        <Default title="Signup" description="Signup description">
+        <main className={`signup__main`}>
             {showSuccess()}
             {showError()}
-            {signUpForm()}
-        </Default>
+            <section className="signup__section">
+                <div className={`signup__content ${success ? 'signup__content--success' : null} ${error ? 'signup__content--error' : null}`}>
+                    <Link to={`/signin`} className={`signup__register`}>Login</Link>
+                    <img src={SpitbossLogo} alt="Spitboss Logo" className={`signup__logo`}/>
+                    <h1 className={`signup__heading`}>Register</h1>
+                    <small className={`signup__small`}>You're free to sign up, but you'll need to prove yourself to become a boss.</small>
+                    {signUpForm()}
+                </div>
+            </section>
+        </main>
     )
 };
 
